41506a5b97b13f7ed20a4437ddd43d61adf785a5
Jonathon Carlyon <JonathonCarlyon@gmail.com>
2013-03-09 03:30:50 -0600

    Finally fixed the bug which caused threads to hang after exceptions while opening URLs.  Added some documentation since the code is getting a little ugly after all that debugging.

--- scripty.py
+++ scripty.py
@@ -1,7 +1,7 @@
 from HTMLParser import HTMLParser 
 from sys import exit
 from base64 import standard_b64decode
-import urllib2, re, os, time, threading, Queue, logging
+import urllib2, re, os, time, threading, Queue, logging, traceback
 
 LOG_FILENAME = 'scripty.out'
 logging.basicConfig(filename=LOG_FILENAME,
@@ -13,7 +13,8 @@ logging.basicConfig(filename=LOG_FILENAME,
 queue = Queue.Queue()
 decode_queue = Queue.Queue()
 
-class DownloadCounter():
+class DownloadTracker():
+  """Responsible for managing download calculations"""
   download_success = 0
   def track_downloads(self, file_count):
     self.file_count = file_count
@@ -23,13 +24,15 @@ class DownloadCounter():
     while True:
       try:
         self.download_count = int(input("How many wallpapers would you like to download? (Must be a multiple of 60): "))
-        if self.download_count % 60 == 0: break
+        if self.download_count % 60 == 0:
+          break
         else:
           print "Please a multiple of 60."
       except:
         print "Please pick a valid number."
  
-class MyHTMLParser(HTMLParser):  
+class MyHTMLParser(HTMLParser):
+  """Checks each href from global page and adds it to decode_queue if it is a wallpaper"""
   def handle_starttag(self, tag, attrs):
     for attr_name, attr_value in attrs:
       if not attr_name == "href": continue
@@ -38,87 +41,99 @@ class MyHTMLParser(HTMLParser):
         decode_queue.put(attr_value)
         
 class TestyClass(HTMLParser):
+  """Selects for base64 encoded image from HTML, decodes it, adds it to queue"""
   def handle_data(self, data):
     base64_imgsrc = re.search(r"(?<=(%s))[\S]*(?=%s)" % (re.escape("'+B(\\'"), re.escape("\\')")), (repr(data)))
     if base64_imgsrc:
       queue.put(standard_b64decode(base64_imgsrc.group()))
               
 class MyThreadedHTMLParser(threading.Thread, HTMLParser):
-  """Decode base64 image and adds the resulting complete URL to queue"""
+  """Passes HTML from URLs in decode_queue to TestyClass"""
   def __init__(self, decode_queue):
     threading.Thread.__init__(self)
     self.decode_queue = decode_queue
   def run(self):
-    url = self.decode_queue.get()
-    html = open_url(url)
-    if html is None: 
-      self.decode_queue.task_done()
-    else:
-      parser2 = TestyClass()
-      parser2.feed(html)
+    try: 
+      url = self.decode_queue.get(True)
+      html = open_url(url)
+      if html is not None: 
+        parser2 = TestyClass()
+        parser2.feed(html)
+    except Queue.Empty:
+      print "decode_queue.get failed"
+    finally:
       self.decode_queue.task_done() 
-    
+      
 class ThreadDownload(threading.Thread):
-  """Threaded Download Attempt"""
+  """Download data from the URL in queue"""
   def __init__(self, queue):
     threading.Thread.__init__(self)
     self.queue = queue 
   def run(self):
-    img_url = self.queue.get()
     try:
-      img_data = urllib2.urlopen(img_url).read()
+      img_url = self.queue.get()
       try:
-        filename = re.search(r"(wallpaper)[\S]+", str(img_url))
-        output = open(str(filename.group()), 'wb')
-        output.write(img_data)
-        output.close()
-       # print "Wallpaper downaloaded to %s!" % (os.path.abspath(filename.group()))
-        DownloadCounter.download_success += 1
-        self.queue.task_done()
-      except IOError as e:
-        print "{} failed to download: {}".format(filename.group(), e.strerror)
-    except urllib2.URLError, e:
-      if hasattr(e, 'reason'):
-        logging.debug('failed to reach a server.')
-        logging.debug('Reason: %s', e.reason)
-        self.queue.task_done()
-        return None
-      elif hasattr(e, 'code'):
-        logging.debug('The serverr couldn\'t fulfill the request.')
-        logging.debug('Code: %s', e.reason)
-        self.queue.task_done()
-        return None
-      else:
-        logging.debug('Shit fucked up1')
-        return None
+        img_data = urllib2.urlopen(img_url).read()
+        try:
+          filename = re.search(r"(wallpaper)[\S]+", str(img_url))
+          output = open(str(filename.group()), 'wb')
+          output.write(img_data)
+          output.close()
+         # print "Wallpaper downaloaded to %s!" % (os.path.abspath(filename.group()))
+          DownloadTracker.download_success += 1
+          self.queue.task_done()
+        except IOError as e:
+          print "{} failed to download: {}".format(filename.group(), e.strerror)
+          return None
+      except urllib2.URLError, e:
+        if hasattr(e, 'reason'):
+          logging.debug('failed to reach a server.')
+          logging.debug('Reason: %s', e.reason)
+          logging.debug(img_url)
+          self.queue.task_done()
+          return None
+        elif hasattr(e, 'code'):
+          logging.debug('The serverr couldn\'t fulfill the request.')
+          logging.debug('Code: %s', e.reason)
+          logging.debug(img_url)        
+          self.queue.task_done()
+          return None
+        else:
+          logging.debug('Something bad happened 1')
+          return None
+    except Queue.Empty:
+      self.queue.task_done()
+      return None
            
 def open_url(url):
   """Opens URL and returns html"""
-  request = urllib2.Request(url)
+  response = urllib2.urlopen(url)
   try:
-    response = urllib2.urlopen(request)
-    link = response.geturl()
     html = response.read()
     response.close()
     return(html)
   except urllib2.URLError, e:
     if hasattr(e, 'reason'):
+      #tb = traceback.format_exc()
+      #print tb
       logging.debug('failed to reach a serverrr.')
       logging.debug('Reason: %s', e.reason)
       logging.debug(url)
       return None
     elif hasattr(e, 'code'):
+      #tb = traceback.format_exc()
+      #print tb
       logging.debug('The serverrrr couldn\'t fulfill the request.')
       logging.debug('Code: %s', e.reason)
+      logging.debug(url)      
       return None
     else:
-      logging.debug('Shit fucked up2')
+      logging.debug('Something bad happened 2')      
       return None    
     
 def mkPicDir(file_path):
   if not os.path.isdir(file_path):
     try:
-      print "sleeping"
       os.mkdir(file_path)
     except OSError, e:
       print "Directory could not be created."#, e.errno
@@ -129,52 +144,38 @@ def main():
   mkPicDir(file_path)
   os.chdir(file_path)
   start_time = time.time()
-  download_counter = DownloadCounter()
-  files_in_file_path = (len([file for file in os.listdir('.') if os.path.isfile(file)]))
-  download_counter.track_downloads(files_in_file_path)
-  download_counter.total_downloads()
+  dt = DownloadTracker()
+  #Count files in ./pics which will be added to current_download_count in order to pick up where the user left off downloading
+  files_in_file_path = (len([file for file in os.listdir('.') if os.path.isfile(file)])) 
+  dt.track_downloads(files_in_file_path)
+  dt.total_downloads()
   while True:
-    url = "http://wallbase.cc/toplist/" + str(download_counter.current_download_count) + "/23/gteq/1920x1080/0/110/60/0"
+    url = "http://wallbase.cc/toplist/" + str(dt.current_download_count) + "/23/gteq/1920x1080/0/110/60/0"
     parser = MyHTMLParser()
     html = open_url(url)
     if html is None: 
-      print "ABRA KADABRA"
+      print "Main() 1st While: html is none"
       continue
+    #First page displays 60 images, this will parse for the appropriate hrefs and add them to decode_queue
     parser.feed(html)
-    #thread pool to decode and retrieve image source 
-    for i in range(60):
-      t = MyThreadedHTMLParser(decode_queue)
-     # t.setDaemon(True)
+    #thread pool
+    for i in range(decode_queue.qsize()):
+      t = MyThreadedHTMLParser(decode_queue)      
       t.start()
     decode_queue.join()
     print "decode_queue joined %.2f" % (time.time() - start_time)
     #thread pool to download 
-    for i in range(60):
+    for i in range(queue.qsize()):
       td = ThreadDownload(queue)
-      #td.setDaemon(True)
       td.start()
     queue.join()
     print "queue joined %.2f" % (time.time() - start_time)
-    download_counter.current_download_count += 60
-    if threading.active_count() >= 150:
-      main_thread = threading.currentThread()
-      for thread in threading.enumerate():
-        if thread is not main_thread:
-          print "Joining :", thread.getName() + " %.2f" % (time.time() - start_time)
-          thread.join(2)
-    print "download_counter.current_download_count: " + str(download_counter.current_download_count - (int(download_counter.download_count) + download_counter.file_count))
-    if download_counter.current_download_count >= (int(download_counter.download_count) + download_counter.file_count):
-      print "Breaking out: " + str(threading.active_count())
-      main_thread = threading.currentThread()
-      for thread in threading.enumerate():
-        print thread.getName()
-        if thread is main_thread: continue
-        print "Joining2 :", thread.getName() + " %.2f" % (time.time() - start_time)
-        while thread.isAlive():
-          print "still alive"
-          thread.join(2)      
-        print threading.active_count()
+    dt.current_download_count += 60
+    print "Images downloaded:" + str(dt.download_success)
+    print "Images left to download: " + str(dt.current_download_count - (int(dt.download_count) + dt.file_count))
+    if dt.current_download_count >= (int(dt.download_count) + dt.file_count):
       break
-  print threading.active_count()
+  if not dt.download_success == dt.download_count:
+    print "Images to retry, once I put in that feature: " + str(dt.download_count - dt.download_success)
   print "%.2f" % (time.time() - start_time)
 main()
